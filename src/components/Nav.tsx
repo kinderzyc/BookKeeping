10ea502ea6e5ea57bd1e2bae798a8cf557274dfe
Your Namekinderzyc <75829215@qq.com>
2020-06-06 22:58:32 +0800

    Icons实现

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import React from "react";
 import { Link } from "react-router-dom";
+require('icons/money.svg');
+require('icons/chart.svg');
+require('icons/tag.svg');
+
 
 const NavWrapper = styled.nav`
 line-height: 24px;
@@ -10,7 +14,15 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
       >li{
         width: 33.333333%;
         text-align: center;
-        padding: 16px;
+        display: flex;
+        padding: 10px 0;
+        flex-direction: column;
+        /* justify-content: center; */
+        align-items: center;
+        >.icon{
+          width: 26px;
+          height: 26px;
+        }
       }
     }
 `;
@@ -20,13 +32,22 @@ const Nav = () => {
         <NavWrapper>
           <ul>
             <li>
-              <Link to="/tags">标签页面</Link>
+            <svg className="icon">
+                 <use xlinkHref="#tag"/>
+              </svg>
+              <Link to="/tags" replace>标签页面</Link>
             </li>
             <li>
-              <Link to="/money">记账页面</Link>
+              <svg className="icon">
+                 <use xlinkHref="#money"/>
+              </svg>
+              <Link to="/money" replace>记账页面</Link>
             </li>
             <li>
-              <Link to="/statistics">统计页面</Link>
+            <svg className="icon">
+                 <use xlinkHref="#chart"/>
+              </svg>
+              <Link to="/statistics" replace>统计页面</Link>
             </li>
           </ul>
         </NavWrapper>
