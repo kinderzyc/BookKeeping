818b548d34f6fcd947f383a8bcc1ae45bdff3b52
Your Namekinderzyc <75829215@qq.com>
2020-06-07 11:19:53 +0800

    Icons封装

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 import React from "react";
 import { Link } from "react-router-dom";
-require('icons/money.svg');
-require('icons/chart.svg');
-require('icons/tag.svg');
+import Icon from "./Icon";
+
 
 
 const NavWrapper = styled.nav`
@@ -28,30 +27,24 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
 `;
 
 const Nav = () => {
-    return (
-        <NavWrapper>
-          <ul>
-            <li>
-            <svg className="icon">
-                 <use xlinkHref="#tag"/>
-              </svg>
-              <Link to="/tags" replace>标签页面</Link>
-            </li>
-            <li>
-              <svg className="icon">
-                 <use xlinkHref="#money"/>
-              </svg>
-              <Link to="/money" replace>记账页面</Link>
-            </li>
-            <li>
-            <svg className="icon">
-                 <use xlinkHref="#chart"/>
-              </svg>
-              <Link to="/statistics" replace>统计页面</Link>
-            </li>
-          </ul>
-        </NavWrapper>
-    )
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Icon name="tag" />
+          <Link to="/tags" replace>标签页面</Link>
+        </li>
+        <li>
+          <Icon name="money" />
+          <Link to="/money" replace>记账页面</Link>
+        </li>
+        <li>
+          <Icon name="chart" />
+          <Link to="/statistics" replace>统计页面</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  )
 }
 
 export default Nav;
