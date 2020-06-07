77ecb31cc3e5d342b83feed69e70abddeb81b7ae
Your Namekinderzyc <75829215@qq.com>
2020-06-07 19:07:06 +0800

    路由图标Hover

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import Layout from 'components/Layout';
+import Tags from 'views/Tags';
+import Money from 'views/Money';
+import Statistics from 'views/Statistics';
+import NoMatch from 'views/NoMatch';
 
 
 function App() {
@@ -31,33 +34,6 @@ function App() {
     </Router>
   );
 }
-function NoMatch() {
-  return <h2>页面不存在！</h2>
-}
 
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
-
-  );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  );
-}
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Icon from "./Icon";
 
 
@@ -13,14 +13,22 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
       >li{
         width: 33.333333%;
         text-align: center;
-        display: flex;
-        padding: 10px 0;
-        flex-direction: column;
-        /* justify-content: center; */
-        align-items: center;
-        >.icon{
-          width: 26px;
-          height: 26px;
+        > a{
+          display: flex;
+          padding: 10px 0;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+            .icon{
+            width: 26px;
+            height: 26px;
+          }
+          &.selected{
+            color: rgb(64, 180, 117);
+            .icon{
+              fill: rgb(64, 180, 117);
+            }
+          }
         }
       }
     }
@@ -31,16 +39,19 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag" />
-          <Link to="/tags" replace>标签页面</Link>
+          <NavLink to="/tags" replace activeClassName="selected">
+            <Icon name="tag" />
+            标签页面</NavLink>
         </li>
         <li>
-          <Icon name="money" />
-          <Link to="/money" replace>记账页面</Link>
+          <NavLink to="/money" replace activeClassName="selected">
+            <Icon name="money" />
+            记账页面</NavLink>
         </li>
         <li>
-          <Icon name="chart" />
-          <Link to="/statistics" replace>统计页面</Link>
+          <NavLink to="/statistics" replace activeClassName="selected">
+            <Icon name="chart" />
+            统计页面</NavLink>
         </li>
       </ul>
     </NavWrapper>
