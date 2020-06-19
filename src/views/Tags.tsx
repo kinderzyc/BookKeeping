2ad72f852d50497ad5b02c60ccd82bca75f839f4
Your Namekinderzyc <75829215@qq.com>
2020-06-19 17:53:41 +0800

    创建/Tags/: Tag路由

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,12 +1,73 @@
 import Layout from "components/Layout";
 import React from "react";
 import { useTags } from "useTags";
+import styled from "styled-components";
+import Icon from "components/Icon";
+import { Link } from "react-router-dom";
+
+const TagList = styled.ol`
+  font-size: 16px;
+  width: 97%;
+  margin: 0 auto;
+  >li{
+    border: 1px solid #fff;
+    line-height: 20px;
+    /* padding: 12px 16px 12px 0; */
+    margin-left: 16px;
+    margin: 10px 10px;
+    border-radius: 6px;
+    background: white;
+    box-shadow: 2px 2px 6px 0px #dfdbdb;
+    >a{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 30px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #00A292;
+  color: white;
+  border-radius: 4px;
+`;
+
+const Center = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Space = styled.div`
+  height: 20px;
+`;
 
 function Tags() {
   const { tags, setTags } = useTags();
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <Link to={'/tags/' + tag}>
+              <span className="oneLine">{tag}</span>
+              <Icon name="right" />
+            </Link>
+          </li>
+        )}
+      </TagList>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button>新增标签</Button>
+      </Center>
+
     </Layout>
   );
 }
