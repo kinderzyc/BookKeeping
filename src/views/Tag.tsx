e49a0207264ee8537ad7ab17268a642b726c33c0
Your Namekinderzyc <75829215@qq.com>
2020-06-22 17:11:25 +0800

    onclick 回退功能

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useTags } from "useTags"
-import { useParams } from "react-router"
+import { useParams, useHistory } from "react-router"
 import Layout from "components/Layout"
 import Icon from "components/Icon"
 import styled from "styled-components"
@@ -8,7 +8,6 @@ import { Button } from "components/Button"
 import { Input } from "components/Input"
 import { Center } from "components/Center"
 import { Space } from "components/Space"
-import { uptime } from "os"
 
 type Params = {
     id: string
@@ -62,10 +61,15 @@ const Tag: React.FC = (props) => {
             </Center>
         </div>
     );
+    const history = useHistory()
+    const onClickBack = () => {
+        history.goBack()
+    }
+
     return (
         <Layout>
             <Header>
-                <Icon name="return" />
+                <Icon name="return" onClick={onClickBack} />
                 <span className="font">编辑标签</span>
                 <Icon />
             </Header>
