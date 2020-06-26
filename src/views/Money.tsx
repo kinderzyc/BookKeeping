6f2539a607559126e2672a6e3c704a7cc29eab71
Your Namekinderzyc <75829215@qq.com>
2020-06-26 22:24:33 +0800

    记账页面持久化修改

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,7 @@ import { TagsSection } from "./Money/TagsSection";
 import { NotesSection } from "./Money/NoteSection";
 import { CategorySection } from "./Money/CategorySection";
 import { NumberPadSection } from "./Money/NumberPadSection";
+import { useRecords } from "hooks/useRecords";
 
 
 type Category = '-' | '+'
@@ -14,16 +15,27 @@ const MyLayout = styled(Layout)`
   flex-direction: column;
 `;
 
+const defaultFormDate = {
+  tagIds: [] as number[],
+  note: '',
+  category: '-' as Category,
+  amount: 0
+};
+
 function Money() {
-  const [selected, setSelected] = useState({
-    tagIds: [] as number[],
-    note: '',
-    category: '-' as Category,
-    amount: 0
-  })
+  const [selected, setSelected] = useState(defaultFormDate);
+  const { records, addRecord } = useRecords();
+  console.log('records');
+  console.log(records);
   const onChange = (obj: Partial<typeof selected>) => {
     setSelected({ ...selected, ...obj })
   }
+  const submit = () => {
+    if (addRecord(selected)) {
+      alert('保存成功')
+      setSelected(defaultFormDate);
+    }
+  };
   return (
     <MyLayout>
       <TagsSection value={selected.tagIds}
@@ -34,9 +46,7 @@ function Money() {
         onChange={category => onChange({ category })} />
       <NumberPadSection value={selected.amount}
         onChange={amount => onChange({ amount })}
-        onok={() => {
-
-        }}
+        onok={submit}
       />
     </MyLayout>
 
